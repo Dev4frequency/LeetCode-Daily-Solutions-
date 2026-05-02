4d9a3042fedd700953248b5dd381bb679d153e6c
YASH BISHT <y.yashbisht2020@gmail.com>
2026-05-02 11:07:35 +0530

    0 ms (100%) 7.9 MB (66.14%) 05-02-2026 0788-rotated-digits Medium .cpp Math

---
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int goodCount = 0;
+        
+        for (int i = 1; i <= n; ++i) {
+            if (isGood(i)) {
+                goodCount++;
+            }
+        }
+        
+        return goodCount;
+    }
+
+private:
+    bool isGood(int num) {
+        bool hasDiffDigit = false;
+        
+        while (num > 0) {
+            int digit = num % 10;
+             if (digit == 3 || digit == 4 || digit == 7) {
+                return false;
+            }
+            if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                hasDiffDigit = true;
+            }
+            
+            num /= 10;
+        }
+         return hasDiffDigit;
+    }
+};
