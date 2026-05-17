afea5eedca7bdfd783fd8d66cc010a9f2253510c
YASH BISHT <y.yashbisht2020@gmail.com>
2026-05-17 09:48:28 +0530

    0 ms (100%) 34.6 MB (99.5%) 05-17-2026 1306-jump-game-iii Medium .cpp Array

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if(start<0 || start>=arr.size()){
+            return false;
+        }
+        if(arr[start]<0){
+            return false;
+        }
+        if(arr[start]==0){
+            return true;
+        }
+        arr[start] = -arr[start];
+
+        return canReach(arr, start + arr[start]) ||
+        canReach(arr, start - arr[start]);
+    }
+};
