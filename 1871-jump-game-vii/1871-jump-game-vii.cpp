ac9e0750ee2984df7680990a25a7615ddf0e6634
YASH BISHT <y.yashbisht2020@gmail.com>
2026-05-25 11:57:49 +0530

    0 ms (100%) 20 MB (88.38%) 05-25-2026 1871-jump-game-vii Medium .cpp String

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.size();
+
+        vector<bool> dp(n, false);
+        dp[0] = true;
+
+        int reachable = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i - minJump >= 0) {
+                reachable += dp[i - minJump];
+            }
+            if (i - maxJump - 1 >= 0) {
+                reachable -= dp[i - maxJump - 1];
+            }
+            if (reachable > 0 && s[i] == '0') {
+                dp[i] = true;
+            }
+        }
+        return dp[n - 1];
+    }
+};
