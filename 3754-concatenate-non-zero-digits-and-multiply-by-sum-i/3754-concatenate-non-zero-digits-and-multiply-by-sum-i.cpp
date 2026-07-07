0043fa329ff6abe99e358af0fa39b56eddee6220
YASH BISHT <y.yashbisht2020@gmail.com>
2026-07-07 11:40:05 +0530

    0 ms (100%) 8.8 MB (89.09%) 07-07-2026 3754-concatenate-non-zero-digits-and-multiply-by-sum-i Easy .cpp Math

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long sum = 0;
+        long long x = 0;
+        long long r = 1;
+        while(n > 0){
+            int d = n % 10;
+            n /= 10;
+            if(d == 0) continue;
+            x = d * r + x;
+            sum += d;
+            r *= 10;
+        }
+        return x * sum;
+    }
+};
