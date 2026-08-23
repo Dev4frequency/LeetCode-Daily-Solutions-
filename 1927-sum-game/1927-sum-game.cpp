34e248ec63d6f78ec86cf631207bce8d6cafccf0
YASH BISHT <y.yashbisht2020@gmail.com>
2026-08-23 16:55:54 +0530

    4 ms (47.55%) 14.1 MB (33.57%) 08-23-2026 1927-sum-game Medium .cpp Math

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <numeric>
+
+using namespace std;
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int sumDiff = 0;
+        int qDiff = 0;
+        for (int i = 0; i < n / 2; ++i) {
+            if (num[i] == '?') {
+                qDiff++;
+            } else {
+                sumDiff += (num[i] - '0');
+            }
+        }
+
+        for (int i = n / 2; i < n; ++i) {
+            if (num[i] == '?') {
+                qDiff--;
+            } else {
+                sumDiff -= (num[i] - '0');
+            }
+        }
+        return (2 * sumDiff != -9 * qDiff);
+    }
+};
