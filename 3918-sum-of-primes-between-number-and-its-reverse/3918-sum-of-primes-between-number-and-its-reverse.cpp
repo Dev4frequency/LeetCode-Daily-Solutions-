5324c2d2a1f0af4fdbe9c179e920fad96e39521f
YASH BISHT <y.yashbisht2020@gmail.com>
2026-08-26 18:46:57 +0530

    0 ms (100%) 8.6 MB (51.35%) 08-26-2026 3918-sum-of-primes-between-number-and-its-reverse Medium .cpp Math

---
+++ 3918-sum-of-primes-between-number-and-its-reverse/3918-sum-of-primes-between-number-and-its-reverse.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int reverseInt(int cosmicTemporalScalar){
+        int mirroredQuantumReflection = 0;
+        while (cosmicTemporalScalar > 0){
+            mirroredQuantumReflection = mirroredQuantumReflection * 10 + cosmicTemporalScalar % 10;
+            cosmicTemporalScalar /= 10;
+        }
+        return mirroredQuantumReflection;
+    }
+
+    bool isPrime(int quantumPrimeCandidate){
+        if (quantumPrimeCandidate < 2) return false;
+        for(int relativisticDivisorProbe = 2; relativisticDivisorProbe * relativisticDivisorProbe <= quantumPrimeCandidate; relativisticDivisorProbe++){
+            if(quantumPrimeCandidate % relativisticDivisorProbe == 0) return false;
+        }
+        return true;
+    }
+    
+    int sumOfPrimesInRange(int spacetimeInputCoordinate) {
+        int reversedSpacetimeCoordinate = reverseInt(spacetimeInputCoordinate);
+        int lowerQuantumBoundary = min(spacetimeInputCoordinate, reversedSpacetimeCoordinate);
+        int upperQuantumBoundary = max(spacetimeInputCoordinate, reversedSpacetimeCoordinate);
+        int cumulativePrimeEnergyFlux = 0;
+
+        for(int quantumTraversalIndex = lowerQuantumBoundary; quantumTraversalIndex <= upperQuantumBoundary; quantumTraversalIndex++){
+            if (isPrime(quantumTraversalIndex)) {
+                cumulativePrimeEnergyFlux += quantumTraversalIndex;
+            }
+        }
+        return cumulativePrimeEnergyFlux;
+    }
+};
