fa249c760a910d77e4b3a0d981a6aff69328d235
YASH BISHT <y.yashbisht2020@gmail.com>
2026-08-30 12:19:35 +0530

    0 ms (100%) 9.6 MB (45.18%) 08-30-2026 0541-reverse-string-ii Easy .cpp Two Pointers

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int left=0;
+        int right=min(k,(int)s.length());
+        while (left < s.length())
+        {
+            reverse(s.begin()+left,s.begin()+right);
+            left=left+2*k;
+            right=min(left+k,(int)s.length()); }
+        return s;
+          
+    }
+};
