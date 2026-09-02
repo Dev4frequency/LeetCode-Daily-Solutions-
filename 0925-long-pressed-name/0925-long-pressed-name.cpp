2ca151e3064622fbe5890ede48c4b7750c031b88
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-02 10:12:53 +0530

    0 ms (100%) 8.8 MB (48.85%) 09-02-2026 0925-long-pressed-name Easy .cpp Two Pointers

---
+++ 0925-long-pressed-name/0925-long-pressed-name.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isLongPressedName(string name, string typed) {
+        int n = name.size();
+        int j = 0;
+        if(j < n && name.size() > typed.size()) return false;
+        for(int i = 0; i < typed.size(); i++){
+            if(typed[i] == name[j]){
+                j++;
+            }else if( i > 0 && typed[i] == typed[i - 1]){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return j == n;
+    }
+};
