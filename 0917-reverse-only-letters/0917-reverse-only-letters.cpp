1d5d789be1890f8a22d7f4b8cab046df99bb6738
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-02 10:17:42 +0530

    0 ms (100%) 8 MB (55.05%) 09-02-2026 0917-reverse-only-letters Easy .cpp Two Pointers

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j)
+        {
+            if((s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z') &&
+            (s[j]>='a' && s[j]<='z' || s[j]>='A' && s[j]<='Z'))
+            {
+                char temp=s[i];
+                s[i]=s[j];
+                s[j]=temp;
+                i++;
+                j--;
+            }
+            else if(!(s[i]>='a' && s[i]<='z' || s[i]>='A' && s[i]<='Z'))
+            {
+                i++;
+            }
+            else if(!(s[j]>='a' && s[j]<='z' || s[j]>='A' && s[j]<='Z'))
+            {
+                j--;
+            }
+        }
+        return s;
+    }
+};
