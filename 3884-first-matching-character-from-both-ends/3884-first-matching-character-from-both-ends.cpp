ab4a0a3007a0254c494fce9021b76a7c98b066b2
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-06 13:09:19 +0530

    0 ms (100%) 9 MB (29.24%) 09-06-2026 3884-first-matching-character-from-both-ends Easy .cpp Two Pointers

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int n = s.length();
+
+        int l =0;
+        int r = n-1;
+
+        while(l <= r){
+            if(s[l] == s[r]){
+                return l;
+            }
+            else{
+                l++;
+                r--;
+            }
+        }
+
+        return -1;
+
+    }
+};
