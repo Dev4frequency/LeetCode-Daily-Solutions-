ba5c9f28fb759d65e13f099bb541330a5db6d1f7
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-07 11:07:26 +0530

    97 ms (39.92%) 38.2 MB (43.28%) 09-07-2026 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers Medium .cpp Array

---
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numTriplets(vector<int>& nums1, vector<int>& nums2) {
+        int count = 0;
+        
+        unordered_map<long long, int> productMap1;
+        unordered_map<long long, int> productMap2;
+        for (int j = 0; j < nums2.size(); ++j) {
+            for (int k = j + 1; k < nums2.size(); ++k) {
+                productMap1[(long long)nums2[j] * nums2[k]]++;
+            }
+        }
+        
+        for (int i = 0; i < nums1.size(); ++i) {
+            long long target = (long long)nums1[i] * nums1[i];
+            if (productMap1.find(target) != productMap1.end()) {
+                count += productMap1[target];
+            }
+        }
+        for (int j = 0; j < nums1.size(); ++j) {
+            for (int k = j + 1; k < nums1.size(); ++k) {
+                productMap2[(long long)nums1[j] * nums1[k]]++;
+            }
+        }
+        
+        for (int i = 0; i < nums2.size(); ++i) {
+            long long target = (long long)nums2[i] * nums2[i];
+            if (productMap2.find(target) != productMap2.end()) {
+                count += productMap2[target];
+            }
+        }
+        
+        return count;
+    }
+};
