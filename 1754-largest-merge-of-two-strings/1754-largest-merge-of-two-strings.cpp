fac5c598fa8b08ded9cdd935893a2aa68f12343c
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-08 11:08:27 +0530

    259 ms (12.56%) 403.8 MB (13.9%) 09-08-2026 1754-largest-merge-of-two-strings Medium .cpp Two Pointers

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string largestMerge(string word1, string word2) {
+        int i = 0, j = 0;
+        string merge = "";
+        while (i < word1.size() && j < word2.size()) {
+            if (word1.substr(i) > word2.substr(j)) {
+                merge += word1[i++];
+            } else {
+                merge += word2[j++];
+            }
+        }
+        return merge + word1.substr(i) + word2.substr(j);
+    }
+};
