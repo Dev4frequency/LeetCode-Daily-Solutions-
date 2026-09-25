0e4166cd00669bd7b3d0762f31bd443de5cd4cb6
YASH BISHT <y.yashbisht2020@gmail.com>
2026-09-25 14:59:33 +0530

    0 ms (100%) 29.7 MB (63.24%) 09-25-2026 0495-teemo-attacking Easy .cpp Array

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) 
+    {
+        if (timeSeries.empty()) return 0;
+
+        int total = 0;
+        for (int i = 0; i < (int)timeSeries.size() - 1; i++) 
+        {
+            total += min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+        total += duration;
+        
+        return total;
+    }
+};
